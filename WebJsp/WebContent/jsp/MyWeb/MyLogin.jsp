937a78925f9c12abf6e84a2c96e1db53634826f9
ShaofengXie <510067760@qq.com>
2016-12-16 09:52:14 +0800

    Signed-off-by: ShaofengXie <510067760@qq.com>

--- WebJsp/WebContent/jsp/MyWeb/MyLogin.jsp
+++ WebJsp/WebContent/jsp/MyWeb/MyLogin.jsp
@@ -4,7 +4,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 
@@ -17,8 +16,8 @@
 	<form action="LoginReceive.jsp">
 		<p style="padding: 30px 0px 10px; position: relative;"><span class="u_logo"></span><input class="ipt" name="userName" type="text" size="18" placeholder="请输入用户名或邮箱" value="" /></p><br /> 
 		<p style="position: relative;"><span class="p_logo"></span><input class="ipt" name="passWord" type="password" size="18" placeholder="请输入密码" value="" /></p><br />
-		<span class="inputcss"><input type="submit" value="提交" name="Submit" />
-		<input type="reset" value="重置" name="Submit1"/></span>
+		<span class="inputcss"><input type="submit" value="提交" name="Submit"/>
+		<input type="reset" value="重置" name="Submit1" /></span>
 	</form>
 	</div>
 
---
+++ WebJsp/src/com/sfx/web/servletlogin/MyLoginServletDemo2.java
@@ -0,0 +1,59 @@
+package com.sfx.web.servletlogin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyLoginServletDemo
+ */
+@WebServlet("/MyWeb/MyLoginServletDemo2")
+public class MyLoginServletDemo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyLoginServletDemo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html;charset=GB2312");//解决乱码问题
+	//response.setCharacterEncoding("GB2312");//解决乱码问题，与上面代码选一
+	request.setCharacterEncoding("UTF-8");
+	String newusername=request.getParameter("userName");
+	String password=request.getParameter("passWord");
+	//newusername=new String(newusername.getBytes("iso8859_1"),"UTF-8");//解决request请求数据乱码
+	if ("xsf".equals(newusername)&&("123456".equals(password))) {
+		ServletContext context=getServletContext();
+		RequestDispatcher rd=context.getRequestDispatcher("");
+		rd.forward(request, response);
+	}else{
+		
+		RequestDispatcher rd=request.getRequestDispatcher("");
+		rd.include(request, response);
+	}
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
